5c7f8443bb16d0c856f1755ec4d7ffa409d21fe5
Jesse Petersen <jesse@petersenmediagroup.com>
2013-11-19 12:08:59 -0500

    committing the plugin files

---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Theme Functions Plugin ===
+Contributors: PMGLLC, peterdog
+Donate link: https://www.petersenmediagroup.com/contribute
+Tags: functions, theme functions, functions.php, ThemeForest, cowboy coding, CPT, CPTs, custom post types
+Requires at least: 3.4.2
+Tested up to: 3.8 alpha
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a theme-dependent area to add functions that must be preserved if the theme is changed (i.e., CPTs).
+
+== Description ==
+
+Protect your dashboard with a game-changing authorization code. The login form will never be the same again.
+
+= Why it exists =
+
+Because ThemeForest continues to push out themes with a crap-ton of features in their themes that all disappear when an unsuspecting user changes themes.
+
+== Installation ==
+
+1. Upload contents of the directory to /wp-content/plugins/ (or use the automatic installer)
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Paste a function or whole crap-ton of functions into theme-functions.php. If you're doing this, you should be someone who knows what the flip you're doing.
+1. No cowboy coding, people.
+
+== Frequently Asked Questions ==
+
+= Where is the UI? =
+
+There is no UI. If you're using this, then you're either a developer or advanced user. Paste a function directly into the only PHP file in the plugin. Save. Upload. Refresh the admin or front-end. It's just like adding a function to functions.php but NOT in the theme.
+
+== Screenshots ==
+
+There's nothing to show. Really.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial stable release.
---
+++ theme-functions-plugin.php
@@ -0,0 +1,88 @@
+<?php
+/*
+Plugin Name: Theme Functions Plugin
+Plugin URI: http://www.petersenmediagroup.com/plugins/
+Description: Adds a theme-dependent area to add functions that must be
+preserved if the theme is changed (i.e., CPTs).
+Version: 1.0.0
+Author: Jesse Petersen
+Author URI: http://www.petersenmediagroup.com
+
+	Copyright 2013 Jesse Petersen
+
+	Limit of liability: Installation and use of this plugin acknowledges the
+  understanding that this program alters WordPress and adds
+  settings to the WordPress database. The author is not responsible for any
+  damages or loss of data that might possibly be incurred through the
+  installation or use of the plugin.
+
+  Support: This is a free plugin, therefore support is limited to bugs that
+  affect all installations. Requests of any other nature will be at the
+  discretion of the plugin author to add or modify the code to account for
+  various installations, servers, or plugin conflicts.
+
+  Licenced under the GNU GPL:
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+// Start up the engine
+class Theme_Functions_Plugin
+{
+
+	/**
+	 * Static property to hold our singleton instance
+	 *
+	 * @return Theme_Functions_Plugin
+	 */
+	static $instance = false;
+
+	/**
+	 * If an instance exists, this returns it.  If not, it creates one and
+	 * returns it.
+	 *
+	 * @return Theme_Functions_Plugin
+	 */
+
+	public static function getInstance() {
+		if ( !self::$instance )
+			self::$instance = new self;
+		return self::$instance;
+	}
+
+	/**
+	 * The actual function(s).
+	 *
+	 * @return Theme_Functions_Plugin
+	 */
+
+	public function add_functions() {
+
+		/**
+		 * Add new functions in this block
+		*/
+
+
+
+		/**
+		 * End functions block
+		 */
+	}
+
+/// end class
+}
+
+// Instantiate our class
+$Theme_Functions_Plugin = Theme_Functions_Plugin::getInstance();
